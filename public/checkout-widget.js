4c39be069c1af4ca7313f0aea01d0423486aee7f
Emie-Trialer <emiedonmukumo@gmail.com>
2025-12-15 18:46:48 +0100

    updated Embed script

--- public/checkout-widget.js
+++ public/checkout-widget.js
@@ -4,12 +4,15 @@
     iframe.src = "https://crypto-checkout-widget-beta.vercel.app/checkout";
     iframe.style.border = "none";
     iframe.style.width = "100%";
-    iframe.style.height = "600px";
+    iframe.style.height = "100%"; // fill container height
     iframe.style.borderRadius = "12px";
 
     const container = document.getElementById("crypto-checkout");
 
     if (container) {
+        // Make container take full viewport if needed
+        container.style.width = "100vw";
+        container.style.height = "100vh";
         container.appendChild(iframe);
     }
 })();
